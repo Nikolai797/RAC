a6e03082f9fe943adaa1cffb976c1b08ff4ce96e
Malikith <l.k.shishkov@gmail.com>
2026-04-02 23:11:30 +0300

    Add files via upload

---
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RentACar.Web1.Areas.Identity.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+        }
+    }
+}
