f61c532d1d9e9a1c1da829ba40a78a4ae2c07667
Malikith <l.k.shishkov@gmail.com>
2026-04-02 23:09:27 +0300

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RentACar.Data.Models;
+
+namespace RentACar.Data.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>()
+                .HasIndex(u => u.EGN)
+                .IsUnique();
+
+            builder.Entity<Car>()
+                .Property(c => c.PricePerDay)
+                .HasPrecision(18, 2);
+        }
+    }
+}
