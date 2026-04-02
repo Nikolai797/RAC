93dec5a3c7506d3d095d4bf73fba650a765dc43c
Malikith <l.k.shishkov@gmail.com>
2026-04-02 23:13:10 +0300

    Add files via upload

---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Web1.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "EGN")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "EGN must be exactly 10 digits.")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "EGN must contain only digits.")]
+        public string EGN { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ ViewModels/ReservationCreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using RentACar.Data.Models;
+
+namespace RentACar.Web1.ViewModels
+{
+    public class ReservationCreateViewModel
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<Car> AvailableCars { get; set; } = new();
+    }
+}
