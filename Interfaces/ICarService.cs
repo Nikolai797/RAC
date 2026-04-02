e2b202502f0282d818e0934a93de7f23f762bcb6
Malikith <l.k.shishkov@gmail.com>
2026-04-02 23:10:43 +0300

    Add files via upload

---
+++ Interfaces/ICarService.cs
@@ -0,0 +1,15 @@
+﻿using RentACar.Data.Models;
+
+namespace RentACar.Services.Interfaces
+{
+    public interface ICarService
+    {
+        Task<IEnumerable<Car>> GetAllCarsAsync();
+        Task<Car?> GetCarByIdAsync(int id);
+        Task AddCarAsync(Car car);
+        Task UpdateCarAsync(Car car);
+        Task DeleteCarAsync(int id);
+        Task<bool> IsCarAvailableAsync(int carId, DateTime startDate, DateTime endDate);
+        Task<IEnumerable<Car>> GetAvailableCarsAsync(DateTime startDate, DateTime endDate);
+    }
+}
---
+++ Interfaces/IReservationService.cs
@@ -0,0 +1,15 @@
+﻿using RentACar.Data.Models;
+
+namespace RentACar.Services.Interfaces
+{
+    public interface IReservationService
+    {
+        Task<IEnumerable<Reservation>> GetAllReservationsAsync();
+        Task<IEnumerable<Reservation>> GetReservationsByUserAsync(string userId);
+        Task<Reservation?> GetReservationByIdAsync(int id);
+        Task CreateReservationAsync(Reservation reservation);
+        Task UpdateReservationAsync(Reservation reservation);
+        Task DeleteReservationAsync(int id);
+        Task<bool> IsCarAvailableForPeriodAsync(int carId, DateTime startDate, DateTime endDate, int? excludeReservationId = null);
+    }
+}
